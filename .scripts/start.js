85069c0dd53821b9f2ebd9181958a7bb288d02ae
Tomi Harju <harjuto@gmail.com>
2016-11-26 22:45:37 +0200

    sdf

--- .scripts/start.js
+++ .scripts/start.js
@@ -40,6 +40,10 @@ var config = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ProgressBarPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
   ]
 }
 
--- sad-esa-react/src/App.js
+++ sad-esa-react/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Websocket from './websocket';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+    constructor() {
+        super();
+        this.state = {
+            animation: 'idle'
+        }
+    }
+
+    updateAnimation(anim) {
+        this.setState({animation: anim})
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <Websocket updateAnimation={this.updateAnimation.bind(this)} />
+                <div className="animstate">
+                  {this.state.animation}
+                </div>
+          </div>
+        );
+    }
 }
 
 export default App;
