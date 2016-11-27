e771875c77938c7578f308222f6fd1d90a5187fb
Elina Nenonen <elina.nenonen@sc5.io>
2016-11-27 12:37:05 +0200

    fix message

--- sad-esa-react/src/Screen.js
+++ sad-esa-react/src/Screen.js
@@ -13,7 +13,7 @@ class Screen extends Component {
   render() {
     return (
       <div className={ `screen ${this.props.animState.animation}`} style={ { background: `url(img/${ this.props.animState.suggestion }.png)`}}>
-        <p className={ `message ${this.props.animState.animation}`} ></p>
+        <p className={ `message ${this.props.animState.animation}`} >PPSSTT!!</p>
         <div className={ `mascot ${this.props.animState.animation}`}></div>
       </div>
     );
