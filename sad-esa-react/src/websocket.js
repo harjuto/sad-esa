a6a22934bc8989b176b7d487fd1dd079a427c1f1
Tomi Harju <harjuto@gmail.com>
2016-11-27 12:57:08 +0200

    clean

--- sad-esa-react/src/websocket.js
+++ sad-esa-react/src/websocket.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-$.connection.hub.url = 'http://esabustop.azurewebsites.net/signalr/hubs';
-var villeProxy = $.connection.busStopHub;
 var video = document.getElementById('video');
 var canvasContext = document.getElementById('canvas').getContext("2d");
 const VIDEO_SAMPLE_RATE = 1000;
@@ -13,17 +11,10 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
         video.src = window.URL.createObjectURL(stream);
     });
-} else {
-    // alert("ERROR PUERTO RICO")
 }
 
 
 export default class WebSocket extends React.Component {
-
-    constructor() {
-        super();
-        this.connection = $.connection.hub;
-    }
 
     componentDidMount() {
         let self = this;
@@ -32,6 +23,7 @@ export default class WebSocket extends React.Component {
                 if (video.paused || video.ended) {
                     return;
                 }
+                console.info("play9ing")
                 canvasContext.drawImage(video, 0, 0, 600, 800);
                 var canvas = document.getElementById('canvas');
                 var dataurl = canvas.toDataURL("image/png");
@@ -42,7 +34,6 @@ export default class WebSocket extends React.Component {
                     array.push(binary.charCodeAt(i));
                 }
                 var imageBlob = new Blob([new Uint8Array(array)], {type: 'image/png'});
-
 
                 var request = new Request("http://esabustopv2.azurewebsites.net/api/busstop", {
                     method: "POST",
@@ -60,32 +51,19 @@ export default class WebSocket extends React.Component {
                 }).catch(function(err) {
                     console.log('Fetch Error :-S', err);
                 });
-                ;
-
-                // villeProxy.server.postImage(binary).done( msg => {
-                // alert(msg)// });
-                // villeProxy.server.imageSent().done( msg => {
-                //     debugger;
-                //     alert(msg);
-                // });
-
             }, VIDEO_SAMPLE_RATE);
         });
 
+        // setTimeout( () => {
+        //     // video.play();
+        // }, 2000)
 
-        this.connection.start()
-          .done(function() {
-              video.play();
-          })
+
     }
 
     render() {
-        var self = this;
-        villeProxy.client.broadcastMessage = function(name, message) {
-            console.info(message)
-            self.props.updateAnimation("found")
-        };
-        return null;
+        return null
+
     }
 
 
